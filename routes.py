27ab5961923d7f7ef2727017217b483a010a7bfe
Aditya Sridhar <aditya123ss@gmail.com>
2014-08-18 22:25:25 -0400

    routes for all links

--- routes.py
+++ routes.py
@@ -2,8 +2,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return render_template('index.html')
+
+@app.route("/photography")
+def photography():
+    return render_template('photography.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
 
 if __name__ == "__main__":
     app.run()
